56cb199c2866b609e597b99429c80c7afb02089a
Katie <kohalloran1215@gmail.com>
2019-04-16 19:52:50 -0500

    Basic Node App Checkpoint

---
+++ node-greeting-app/farewell.js
@@ -0,0 +1,7 @@
+exports.goodbyeFrench = function(){
+  console.log("Au revoir");
+}
+
+exports.goodbyeMandarin = function(){
+  console.log("再见");
+}
---
+++ node-greeting-app/main.js
@@ -0,0 +1,16 @@
+const greetings = require("./greeting.js")
+const bye = require("./farewell.js")
+//Hello
+greetings.greetingEnglish();
+
+//Konichiwa
+greetings.greetingJapanese();
+
+//Hola
+greetings.greetingSpanish();
+
+//Halló
+greetings.greetingIcelandic();
+
+//再见
+bye.goodbyeMandarin();
